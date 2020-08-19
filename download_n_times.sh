9df7f58a35ac3561dd520cbb858adc8586655568
Adarsh K G <42889982+adarshgeorge@users.noreply.github.com>
2020-08-20 03:06:44 +0530

    Update download_n_times.sh

--- download_n_times.sh
+++ download_n_times.sh
@@ -5,7 +5,7 @@ n=1
 # continue until $n equals 5
 while [ $n -le 2 ]
 do
-        echo "Welcome $n times."
+        echo "Downloading $n times."
         wget http://nycmetro-speedtest.reliablesite.net/1000mb.test.zip
         n=$(( n+1 ))     # increments $n
 done
